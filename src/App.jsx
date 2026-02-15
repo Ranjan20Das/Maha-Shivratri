3d4e3683882664731e69373e9146dee0f303e8cd
Ranjan20Das <daschaya656@gmail.com>
2026-02-15 18:47:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom"
+import React from "react";
+
 import Header from "./components/Header"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
